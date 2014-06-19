e868fc041efa8ec69fa70d05e47e85df9ac853b5
Aaron France <aaron.l.france@gmail.com>
2014-06-19 12:41:44 +0200

    Add specs.

--- src/ets_manager.erl
+++ src/ets_manager.erl
@@ -93,12 +93,13 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% @doc Create or return an ets table for use. It will make the ets_manager
 %% a heir on the table so that on failure it is returned to it.
--spec give_me (Name::ets:tab(), pid(), state()) -> {ok, ets:tid()} | {error, term()}.
+-spec give_me (Name::ets:tab(), pid(), state()) ->
+    {ok, ets:tid()} | {error, term()}.
 give_me(Name, Pid, State) ->
     give_me(Name, [], Pid, State).
 
--spec give_me (Name::ets:tab(), [term()], pid(), state())
-  -> {ok, ets:tid()} | {error, already_own_table}.
+-spec give_me (Name::ets:tab(), [term()], pid(), state()) ->
+    {ok, ets:tab()} | {error, already_own_table}.
 give_me(Name, Opts, Pid, State) ->
     Me = self(),
     case ets:info(Name) of
